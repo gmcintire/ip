706f2d9e1b2d77683966ec54aa5088bde66bf654
Graham McIntire <graham@mcintire.me>
2022-06-12 14:45:15 -0500

    show ip if proxied too

--- lib/ip_web/controllers/page_controller.ex
+++ lib/ip_web/controllers/page_controller.ex
@@ -6,6 +6,17 @@ defmodule IpWeb.PageController do
     # {:ok,
     # {:hostent, 'lax28s10-in-f14.1e100.net', [], :inet, 4, [{172, 217, 5, 206}]}}
 
-    render(conn, "index.html")
+    forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
+
+    remote_ip =
+      if forwarded_for do
+        String.split(forwarded_for, ",")
+        |> Enum.map(&String.trim/1)
+        |> List.first()
+      else
+        to_string(:inet_parse.ntoa(conn.remote_ip))
+      end
+
+    render(conn, "index.html", remote_ip: remote_ip)
   end
 end
