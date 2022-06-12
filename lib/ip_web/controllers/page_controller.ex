9642319280a9dbf5c6dbbec0b4eb8f3a01f4f3de
Graham McIntire <graham@mcintire.me>
2022-06-12 12:07:35 -0500

    actually show ip

--- lib/ip_web/controllers/page_controller.ex
+++ lib/ip_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule IpWeb.PageController do
   use IpWeb, :controller
 
   def index(conn, _params) do
+    # iex(1)> :inet_res.gethostbyaddr({172, 217, 5, 206})
+    # {:ok,
+    # {:hostent, 'lax28s10-in-f14.1e100.net', [], :inet, 4, [{172, 217, 5, 206}]}}
+
     render(conn, "index.html")
   end
 end
