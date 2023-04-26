eae74b56d1985d4f0f662e3bc9f34dd2c5d95e1c
Graham McIntire <graham@mcintire.me>
2023-04-26 09:38:53 -0500

    change remote ip detection

--- lib/ip_web/controllers/page_controller.ex
+++ lib/ip_web/controllers/page_controller.ex
@@ -6,16 +6,31 @@ defmodule IpWeb.PageController do
     # {:ok,
     # {:hostent, 'lax28s10-in-f14.1e100.net', [], :inet, 4, [{172, 217, 5, 206}]}}
 
-    forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
+    #forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
 
-    remote_ip =
-      if forwarded_for do
-        String.split(forwarded_for, ",")
-        |> Enum.map(&String.trim/1)
-        |> List.first()
-      else
-        to_string(:inet_parse.ntoa(conn.remote_ip))
-      end
+    #remote_ip =
+    #  if forwarded_for do
+    #    String.split(forwarded_for, ",")
+    #    |> Enum.map(&String.trim/1)
+    #    |> List.first()
+    #  else
+    #    to_string(:inet_parse.ntoa(conn.remote_ip))
+    #  end
+
+    forwarded_for = conn
+      |> Plug.Conn.get_req_header("x-forwarded-for")
+      |> List.first()
+
+    remote_ip = if forwarded_for do
+      forwarded_for
+      |> String.split(",")
+      |> Enum.map(&String.trim/1)
+      |> List.first()
+    else
+      conn.remote_ip
+      |> :inet_parse.ntoa()
+      |> to_string()
+    end
 
     render(conn, "index.html", remote_ip: remote_ip)
   end
