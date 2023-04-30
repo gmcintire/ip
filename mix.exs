224ca9440c2ee6c979746cc6e06934842c4aadf5
Graham McIntire <graham@mcintire.me>
2023-04-30 13:03:25 -0500

    deploy testing

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Ip.MixProject do
   def application do
     [
       mod: {Ip.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
